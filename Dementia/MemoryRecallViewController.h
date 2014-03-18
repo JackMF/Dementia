b151696da6695cd92b7e0f61cd99089235ded6fa
JackMF <jackmckayfletcher@gmail.com>
2014-03-18 15:17:02 +0000

    starting test 3

---
+++ Dementia/MemoryRecallViewController.h
@@ -0,0 +1,18 @@
+//
+//  MemoryRecallViewController.h
+//  Dementia
+//
+//  Created by Jack Fletcher on 18/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestViewController.h"
+
+@interface MemoryRecallViewController : TestViewController
+{
+    
+    __weak IBOutlet UILabel *Story;
+}
+
+@end
--- Dementia/MemoryRecallViewController.m
+++ Dementia/MemoryRecallViewController.m
@@ -1,18 +1,19 @@
 //
-//  ExampleTestViewController.m
+//  MemoryRecallViewController.m
 //  Dementia
 //
-//  Created by Chris on 16/03/2014.
+//  Created by Jack Fletcher on 18/03/2014.
 //  Copyright (c) 2014 chris. All rights reserved.
 //
 
-#import "ExampleTestViewController.h"
+#import "MemoryRecallViewController.h"
+#import "Test.h"
 
-@interface ExampleTestViewController ()
+@interface MemoryRecallViewController ()
 
 @end
 
-@implementation ExampleTestViewController
+@implementation MemoryRecallViewController
 @synthesize test;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -20,33 +21,20 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        self.title = @"Example Test";
     }
     return self;
 }
 
 - (void)viewDidLoad
 {
-    currentScore = 0;
     [super viewDidLoad];
-    [infoLabel setText:@"Press the button below to finish the test"];
     // Do any additional setup after loading the view from its nib.
-}
-
--(IBAction)finishButtonPressed
-{
-    [super hasFinishedTestWithScore:currentScore];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-- (IBAction)pressMeButtonPressed:(id)sender {
-    currentScore++;
-    NSLog(@"Score: %i", currentScore);
-    
 }
 
 @end
