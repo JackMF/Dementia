8630edcc10d152771bc7995f8950bbb8abbc128d
JackMF <jackmckayfletcher@gmail.com>
2014-03-26 13:23:34 +0000

    Test.plist - data added for alternation test

---
+++ Dementia/ExecutiveAlternationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExecutiveAlternationViewController.h
+//  Dementia
+//
+//  Created by Jack Fletcher on 25/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface ExecutiveAlternationViewController : TestViewController
+
+@end
---
+++ Dementia/ExecutiveAlternationViewController.m
@@ -0,0 +1,38 @@
+//
+//  ExecutiveAlternationViewController.m
+//  Dementia
+//
+//  Created by Jack Fletcher on 25/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import "ExecutiveAlternationViewController.h"
+
+@interface ExecutiveAlternationViewController ()
+
+@end
+
+@implementation ExecutiveAlternationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
